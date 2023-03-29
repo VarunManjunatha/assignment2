f31e06eed91275f41ab09dc83dfd278815458f57
VarunManjunatha <126263224+VarunManjunatha@users.noreply.github.com>
2023-03-29 04:22:39 -0700

    Update splitter.sol

--- splitter.sol
+++ splitter.sol
@@ -1,9 +1,11 @@
+//0xcE055a0F5E58a415D0dfff5dD5c72BCead7E4512
+
 pragma solidity ^0.8.0;
 
 contract paymentsplit {
     function split(address payable user1,address payable user2) external payable{
         require(msg.value %2==0 && msg.value !=0, "value must be even and non-zero");
-        uint splitvalue = msg.value / 2;
+        uint256 splitvalue = msg.value / 2;
         user1.transfer(splitvalue);
         user2.transfer(splitvalue);
     }
